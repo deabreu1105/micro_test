cb1b2fc3a66f98a27fe0263589e8278ba34afff7
deabreu1105 <deabreu83@gmail.com>
2023-07-09 21:23:19 -0500

    Subida de mis cambios servicio Daniel

---
+++ micro_daniel/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const cors = require('cors');
+app.use(cors());
+app.use(express.json())
+
+
+app.get("/api/serverDaniel", async(req, res) => {
+
+    
+  
+    return res.status(200).json({message:'Hola mundo'});
+
+});
+
+ 
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
+function closeServer() {
+  server.close();
+}  
+module.exports = { app, closeServer };
